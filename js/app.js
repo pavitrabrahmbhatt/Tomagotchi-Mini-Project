9a89e7638b90af3b5be6ea1cd04d4323617ef7c1
Pavitra Brahmbhatt <pavitrabrahmbhatt@Pavitras-MacBook-Air.local>
2019-07-02 15:09:43 -0500

    added buttons and game object

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 const tomagotchiName = prompt("Please enter your name", "Name of your Tomagotchi");
 
+class Tomagotchi {
+	constructor(tomagotchiName) {
+		this.name = tomagotchiName;
+		this.foodLevel = 10;
+    	this.playLevel = 10;
+    	this.sleepLevel = 10;
+	}
+}
 
+const game = {
+
+}
