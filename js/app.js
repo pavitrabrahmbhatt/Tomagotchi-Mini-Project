646089078a99735292818037cb13da260cdd2eab
Pavitra Brahmbhatt <pavitrabrahmbhatt@Pavitras-MacBook-Air.local>
2019-07-03 10:53:00 -0500

    added methods to class

--- js/app.js
+++ js/app.js
@@ -8,25 +8,79 @@ class Tomagotchi {
     	this.playLevel = 10;
     	this.sleepLevel = 10;
 	}
+
+	feed(player) {
+ 		player.foodLevel += 1;
+ 		return this.foodLevel
+ 	}
+
+ 	sleep(player) {
+ 		player.sleepLevel += 1;
+ 		return this.sleepLevel
+ 	}
+
+ 	play(player) {
+ 		player.playLevel += 1;
+ 		return this.playLevel
+ 	}
 }
 
 
 //instatiate the tomagotchi
-const yourTomagotchi = new Tomagotchi(tomagotchiName)
 
-console.log(yourTomagotchi);
 
-	timePasses = function () {
+
+
+
+const game = {
+
+	makeTomagatchi: function () {
+		const yourTomagotchi = new Tomagotchi()
+		this.player = yourTomagotchi
+	},
+
+	timePasses: function () {
 	    this.foodLevel = this.foodLevel - 1;
 	    this.playLevel = this.playLevel - 1;
 	    this.sleepLevel = this.sleepLevel - 1;
+	},
+
+
+	isLiving: function () {
+	    if (this.foodLevel <= 0 || this.playLevel <= 0 || this.sleepLevel <= 0) {
+	      return false;
+	    } else {
+	      return true;
+	    }
 	}
 
- 
-isLiving = function () {
-    if (this.foodLevel <= 0 || this.playLevel <= 0 || this.sleepLevel <= 0) {
-      return false;
-    } else {
-      return true;
-    }
 }
+
+
+game.makeTomagatchi()
+
+
+
+
+
+
+// $("#food-button").click(function() {
+//     tamagotchi.foodUp();
+//     $("span#food-level").text(tamagotchi.foodLevel);
+//   })
+//   $("#play-button").click(function() {
+//     tamagotchi.playUp();
+//     $("span#play-level").text(tamagotchi.playLevel);
+//   })
+//   $("#sleep-button").click(function() {
+//     tamagotchi.sleepUp();
+//     $("span#sleep-level").text(tamagotchi.sleepLevel);
+//   })
+
+
+
+
+
+
+
+
